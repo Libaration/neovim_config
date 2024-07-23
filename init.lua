dce962544b019c72de7bebedd85adf89c759762f
Cristian Deleon <cristian.deleon@kroger.com>
2024-07-23 09:50:52 -0400

    update which-key

--- init.lua
+++ init.lua
@@ -662,15 +662,18 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+
+local wk = require("which-key")
+wk.add({
+  { "", group = "[C]ode" },
+  { "", group = "[S]earch" },
+  { "", group = "[G]it" },
+  { "", group = "[W]orkspace" },
+  { "", group = "[R]ename" },
+  { "", group = "[D]ocument" },
+  { "", group = "More git" },
+  { "", desc = "",            hidden = true, mode = { "n", "n", "n", "n", "n", "n", "n" } },
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
